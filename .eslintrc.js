06b2cb1141cfe0873a2e36fc28c8e4d3264dc5e0
Amir Haghighati <haghighati.amir@gmail.com>
2020-06-17 12:17:35 -0400

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,14 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
+    es6: true,
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
-  extends: [
-    'plugin:prettier/recommended',
-    'prettier',
-  ],
-  plugins: [
-    'prettier'
-  ],
+  extends: ['plugin:prettier/recommended', 'prettier'],
+  plugins: ['prettier'],
   // add your custom rules here
-  rules: {
-  }
+  rules: {},
 }
